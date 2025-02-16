67ac38fa78885e686fef3e7a52056ab2644d4542
nishant gupta <nishantgupta7995@gmail.com>
2025-02-16 17:56:43 +0530

    Revert "created demo about and contact us page"
    This reverts commit 0a34ae30f272528901c19728fc42b8c1c80f8299.

--- app/components/Sliders.jsx
+++ app/components/Sliders.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@nextui-org/react";
+import { Heart } from "lucide-react";
 import Link from "next/link";
 import Slider from "react-slick";
 import FavoriteButton from "./FavoriteButton";
