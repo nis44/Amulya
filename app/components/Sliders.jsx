19f54aa99f7bbeed557722df90044a054c2dcfa5
nishant gupta <nishantgupta7995@gmail.com>
2025-02-16 17:51:51 +0530

    Revert "Update Next.js version and remove unused screen setting in FeaturedProductSlider"
    This reverts commit 7cc79b64a2ea64ba02eb96f7d544178bfd6369ac.

--- app/components/Sliders.jsx
+++ app/components/Sliders.jsx
@@ -8,6 +8,7 @@ import AddToCartButton from "./AddToCartButton";
 
 export default function FeaturedProductSlider({ featuredProducts }) {
   var settings = {
+    screen:1,
     dots: true,
     infinite: true,
     speed: 500,
