7e01ae71b0f912f542a869d61ecac0f90405f2d9
sanchitwiz <sanchitvohra13@gmail.com>
2025-02-17 20:05:33 +0530

    Add about-us and contact-us pages

---
+++ app/(pages)/about-us/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-10'>
+      About us page
+    </div>
+  )
+}
+
+export default page
---
+++ app/(pages)/contact-us/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-10'>
+      Contact us page
+    </div>
+  )
+}
+
+export default page
